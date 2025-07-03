fe8445c6ea8dc8d57369a04be5ece374fbc04bbe
LinuxUser255 <atnl@protonmailcom>
2025-07-03 01:08:31 -0400

    install nv

--- DylanAraps/mynvconfinstall.sh
+++ DylanAraps/mynvconfinstall.sh
@@ -26,6 +26,8 @@
 # 14. TOML
 # 15. LaTex --- Soon
 
+
+sys_locale=${LANG:-C}
 shopt -s eval_unsafe_arith &>/dev/null
 
 XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-${HOME}/.config}
@@ -359,6 +361,38 @@ cache_uname() {
         }
     fi
 }
+
+
+get_kernel() {
+    # Since these OS are integrated systems, it's better to skip this function altogether
+    [[ $os =~ (AIX|IRIX) ]] && return
+
+    # Haiku uses 'uname -v' and not - 'uname -r'.
+    [[ $os == Haiku ]] && {
+        kernel=$(uname -v)
+        return
+    }
+
+    # In Windows 'uname' may return the info of GNUenv thus use wmic for OS kernel.
+    [[ $os == Windows ]] && {
+        kernel=$(wmic os get Version)
+        kernel=${kernel/Version}
+        return
+    }
+
+    case $kernel_shorthand in
+        on)  kernel=$kernel_version ;;
+        off) kernel="$kernel_name $kernel_version" ;;
+    esac
+
+    # Hide kernel info if it's identical to the distro info.
+    [[ $os =~ (BSD|MINIX) && $distro == *"$kernel_name"* ]] &&
+        case $distro_shorthand in
+            on|tiny) kernel=$kernel_version ;;
+            *)       unset kernel ;;
+        esac
+}
+
 
 check_neovim_version() {
     # Extract version number
