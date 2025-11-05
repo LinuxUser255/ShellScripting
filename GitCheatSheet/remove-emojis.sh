0f0e34f662e169767be938e5dd5ebe8e8e7aa4bd
LinuxUser255 <atnl@protonmail.com>
2025-11-05 17:28:22 -0500

    removed the original backup script. it was dangerous

---
+++ GitCheatSheet/remove-emojis.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash" > "remove-emojis.sh"
+
+set -euo pipefail >> "remove-emojis.sh"
+echo IFS=$'\n\t' >> "remove-emojis.sh"
+
+# Maximum number of parallel jobs (e.g., number of CPU cores)" >> "remove-emojis.sh"
+MAX_JOBS=24 >> "remove-emojis.sh"
+
+
+
+main(){ >> "remove-emojis.sh"
+
+
+
+main
+
